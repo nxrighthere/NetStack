9e640b35a97d5855afb7b09951e46d584ac2764c
NX <nxrighthere@gmail.com>
2018-11-29 01:50:19 +0500

    Minor formatting

--- Source/NetStack.Threading/ConcurrentBuffer.cs
+++ Source/NetStack.Threading/ConcurrentBuffer.cs
@@ -44,10 +44,10 @@ namespace NetStack.Threading {
 
 		public ConcurrentBuffer(int bufferSize) {
 			if (bufferSize < 2)
-                throw new ArgumentException("bufferSize");
+				throw new ArgumentException("bufferSize");
 
 			if ((bufferSize & (bufferSize - 1)) != 0)
-                throw new ArgumentException("bufferSize");
+				throw new ArgumentException("bufferSize");
 
 			_bufferMask = bufferSize - 1;
 			_buffer = new Cell[bufferSize];
