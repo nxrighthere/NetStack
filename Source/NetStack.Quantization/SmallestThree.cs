831103c63f86d444815ff9f0dc9ed25d8ef180ef
Stanislav Denisov <nxrighthere@gmail.com>
2020-06-08 09:22:25 +0500

    Update copyright

--- Source/NetStack.Quantization/SmallestThree.cs
+++ Source/NetStack.Quantization/SmallestThree.cs
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018 Stanislav Denisov
+ *  Copyright (c) 2020 Stanislav Denisov, Maxim Munning, Davin Carten
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
